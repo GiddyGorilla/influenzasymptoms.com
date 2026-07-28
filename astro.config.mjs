f0beec859721e7e55cf75a8cab6573ee542ada37
Patri <patri@influenzasymptoms.com>
2026-07-28 02:52:25 +0200

    Add GitHub Actions for auto-deploy to Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://influenzasymptoms.com',
+  integrations: [sitemap()],
   adapter: cloudflare(),
 
   vite: {
